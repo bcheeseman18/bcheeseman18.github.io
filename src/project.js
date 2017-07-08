4d3831ae33718f557b0cbd7a182761b2a4c667e1
bcheeseman18 <benjamincheeseman18@gmail.com>
2017-07-08 00:38:30 -0400

    responsiveness

--- src/project.js
+++ src/project.js
@@ -4,11 +4,16 @@ class Project extends Component {
     render() {
         return (
             <div className="App">
-                <h2>{this.props.details.name}</h2>
-                <p>{this.props.details.technology}</p>
-                <a href={this.props.details.url}><img className="projImage" src={this.props.details.image} alt='map'/></a>
-                <p>{this.props.details.description}</p>
-                <p>{this.props.details.date}</p>
+                <div className="container">
+                        <div className="image">
+                            <h2>{this.props.details.name}</h2>
+                            <a href={this.props.details.url}><img className="projImage" src={this.props.details.image} alt='map'/></a>
+                        </div>
+                            <div className="projInfo">
+                                <p>{this.props.details.description}</p>
+                                <p>{this.props.details.technology}</p>
+                            </div>
+                </div>
             </div>
         );
     }
